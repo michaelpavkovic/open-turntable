365d2e70d320ca042f03c8e3bfcf9d7948947766
Michael Pavkovic <mpavkovic688@gmail.com>
2020-11-01 16:26:44 -0500

    Gitignore, env, and main

---
+++ env.py
@@ -0,0 +1,4 @@
+"""
+This is the place to store personal environment variables, such as the path to GCC ARM.
+"""
+GCC_ARM_PATH = ""
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "mbed.h"
+
+
+int main() {
+	std::cout << "test" << std::endl;
+}
